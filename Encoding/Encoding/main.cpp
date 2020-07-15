080fce66f78581fc81a7c1564e48256c40da7948
testpilot441 <54686642+testpilot441@users.noreply.github.com>
2020-07-14 20:44:30 -0400

    Add files via upload

--- Encoding/Encoding/main.cpp
+++ Encoding/Encoding/main.cpp
@@ -14,109 +14,12 @@ int main()
 	char array[] = { '0','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z' };
 	for (int i = 0; i < size(s); i++) //iterating through the string over here
 	{
-		if(s[i] == array[1])
+		for (int j = 1; j < 27; j++)
 		{
-			cout << 1;
-		}
-		if (s[i] == array[2])
-		{
-			cout << 2;
-		}
-		if (s[i] == array[3])
-		{
-			cout << 3;
-		}
-		if (s[i] == array[4])
-		{
-			cout << 4;
-		}
-		if (s[i] == array[5])
-		{
-			cout << 5;
-		}
-		if (s[i] == array[6])
-		{
-			cout << 6;
-		}
-		if (s[i] == array[7])
-		{
-			cout << 7;
-		}
-		if (s[i] == array[8])
-		{
-			cout << 8;
-		}
-		if (s[i] == array[9])
-		{
-			cout << 9;
-		}
-		if (s[i] == array[10])
-		{
-			cout << 10;
-		}
-		if (s[i] == array[11])
-		{
-			cout << 11;
-		}
-		if (s[i] == array[12])
-		{
-			cout << 12;
-		}
-		if (s[i] == array[13])
-		{
-			cout << 13;
-		}
-		if (s[i] == array[14])
-		{
-			cout << 14;
-		}
-		if (s[i] == array[15])
-		{
-			cout << 15;
-		}
-		if (s[i] == array[16])
-		{
-			cout << 16;
-		}
-		if (s[i] == array[17])
-		{
-			cout << 17;
-		}
-		if (s[i] == array[18])
-		{
-			cout << 18;
-		}
-		if (s[i] == array[19])
-		{
-			cout << 19;
-		}
-		if (s[i] == array[20])
-		{
-			cout << 20;
-		}
-		if (s[i] == array[21])
-		{
-			cout << 21;
-		}
-		if (s[i] == array[22])
-		{
-			cout << 22;
-		}
-		if (s[i] == array[23])
-		{
-			cout << 23;
-		}
-		if (s[i] == array[24])
-		{
-			cout << 24;
-		}
-		if (s[i] == array[25])
-		{
-			cout << 25;
-		}
-		if (s[i] == array[26])
-		{
-			cout << 26;
+			if (s[i] == array[j])
+			{
+				cout << j;
+			}
 		}
 	}
 	cout << " " << endl;
