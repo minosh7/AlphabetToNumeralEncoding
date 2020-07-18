8f199bb5bda1682cc7a5b70f3372291c7815ffd6
testpilot441 <54686642+testpilot441@users.noreply.github.com>
2020-07-18 18:07:01 -0400

    Add files via upload

--- Encoding/Encoding/main.cpp
+++ Encoding/Encoding/main.cpp
@@ -7,22 +7,41 @@ using namespace std;
 
 int main()
 {
-	string s;
-	cout << "Take an input to map your character:" << endl;
-	cin >> s;
-	
-	char array[] = { '0','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z' };
-	for (int i = 0; i < size(s); i++) //iterating through the string over here
+	char choice;
+	do
 	{
-		for (int j = 1; j < 27; j++)
+		string s;
+		cout << "Input Sequence:" << endl;
+		getline(cin >> ws, s);
+
+		char array[] = { '0','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z' };
+		cout << " " << endl;
+		cout << "Encoded:" << endl;
+		for (int i = 0; i < size(s); i++) //iterating through the string over here
 		{
-			if (s[i] == array[j])
+			if (s[i] == ' ')
 			{
-				cout << j;
+				cout << ' ';
+			}
+			for (int j = 1; j < 27; j++) //at the same time, iterate through the alphabet
+			{
+				if (s[i] == array[j]) // if the letter in string is equal to the letter in the alphabet we found, then, return the respecive number
+				{
+					cout << j;
+				}
 			}
 		}
+		cout << " " << endl;
+		cout << "Retry encoding? (Y/N):" << endl; //Try a different input
+		cout << " " << endl;
+		cin >> choice;
+		cout << " " << endl;
+	} 
+	while (choice == 'Y' || choice == 'y'); //keep doing this so long as the choice is yes.
+	if (choice == 'N' || choice == 'n')
+	{
+		exit;
 	}
-	cout << " " << endl;
-	system("pause");
+	
 	return 0;
 }
